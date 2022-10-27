ea936e4931be9401c4acec85d0a11a00cdd90d0d
Jonathon <jwbialas@gmail.com>
2022-10-27 08:22:23 -0500

    buttons connected

--- client/src/VideoPage.js
+++ client/src/VideoPage.js
@@ -8,8 +8,12 @@ function VideoPage({watch, user}){
     const [subscribing, setSubscribing] = useState(false)
     const [newComment, setNewComment] = useState(false)
     const [likesArray, setLikesArray] = useState([])
+    const [dislikesArray, setDislikesArray] = useState([])
     const [subscriptionsArray, setSubscriptionsArray] = useState([])
     const [description, setDescription] = useState('')
+    const [numberOfLikes, setNumberOfLikes] = useState(0)
+    const [numberOfDislikes, setNumberOfDislikes] = useState(0)
+
 
 
     useEffect(()=>{
@@ -21,20 +25,58 @@ function VideoPage({watch, user}){
     useEffect(()=>{
         fetch("/likes")
         .then(r=>r.json())
-        .then(r=>setLikesArray(r))
+        .then(r=>{
+            setLikesArray(r.find((like)=>{
+            return(like.video.id === watch.id && like.user.id === user.id)
+        }));
+            setNumberOfLikes(r.length);
+        if (likesArray.length>0) {
+            setUpVote(true)
+        }
+    })
     },[])
 
     useEffect(()=>{
         fetch("/subscriptions")
         .then(r=>r.json())
-        .then(r=>setSubscriptionsArray(r))
+        .then(r=>{
+            setSubscriptionsArray(r.find((subscription)=>{
+            return(subscription.video.id === watch.id && subscription.user.id === user.id)
+        }));
+        if (subscriptionsArray.length>0) {
+            setSubscribing(true)
+        }
+    })
+    },[])
+
+    useEffect(()=>{
+        fetch("/dislikes")
+        .then(r=>r.json())
+        .then(r=>{
+            setDislikesArray(r.find((dislike)=>{
+            return(dislike.video.id === watch.id && dislike.user.id === user.id)
+        }));
+            setNumberOfDislikes(r.length);
+        if (dislikesArray.length>0) {
+            setDownVote(true)
+        }
+    })
     },[])
 
     function subscribe(){
+        if (subscribing === false){
+            //post to  subscriptions
+            fetch("/subscriptions",{
+                method: "POST",
+                headers:{"Content-Type":"Application/json"},
+                body: JSON.stringify({user_id: user.id, video_id: watch.id})
+                })
+                .then(r=>r.json())
+        } else {
+            fetch(`/subscriptions/${subscriptionsArray[0].id}`, {method: "DELETE"})
+           .then(r=>r.json())
+        }
         setSubscribing(!subscribing)
-        //check if subscribed
-        //conditionaly say subscribe or unsubscribe
-        //fetch().then().then()
     }
 
     function commenting(){
@@ -42,23 +84,48 @@ function VideoPage({watch, user}){
     }
 
     function like(){
-        setUpVote(!upVote)
-        if (downVote === true){
+        if (upVote === true){        
+            //delete upvote
+            fetch(`/likes/${likesArray[0].id}`, {method: "DELETE"})
+           .then(r=>r.json())
+         } else {
+            //post new like
+             fetch("/likes/",{
+                method: "POST",
+                headers:{"Content-Type":"Application/json"},
+                body: JSON.stringify({user_id: user.id, video_id: watch.id})
+                })
+                .then(r=>r.json())
+            if (downVote === true){
             setDownVote(false)
+            //delete downVote
+             fetch(`/dislikes/${dislikesArray[0].id}`, {method: "DELETE"})
+             .then(r=>r.json())
         }
-        //check if liked 
-        //conditionally show number of likes or number + 1
-        //fetch().then().then()
-    }
+        setUpVote(!upVote)
+    }}
 
     function dislike(){
-        setDownVote(!downVote)
-        if (upVote === true){
+        if (downVote === true){        
+            //delete downvote
+             fetch(`/dislikes/${dislikesArray[0].id}`, {method: "DELETE"})
+             .then(r=>r.json())
+         } else {
+            //post new dislike
+             fetch("/dislikes",{
+                method: "POST",
+                headers:{"Content-Type":"Application/json"},
+                body: JSON.stringify({user_id: user.id, video_id: watch.id})
+                })
+                .then(r=>r.json())
+            if (upVote === true){
             setUpVote(false)
+            //delete upVote
+            fetch(`/likes/${likesArray[0].id}`, {method: "DELETE"})
+            .then(r=>r.json())
         }
-        //check if liked 
-        //conditionally show number of likes or number + 1
-        //fetch().then().then()
+        setUpVote(!upVote)
+    }
     }
       const commentArray = watch.comments.map(comment=>{
         return(<Comments key={comment.id} comment={comment}/>)
@@ -80,6 +147,7 @@ function VideoPage({watch, user}){
             <h2>{watch.title}</h2>
             <iframe width="710" height="400" src={watch.src}></iframe><br></br>
             <p>Description: {watch.description}</p>
+            <div><p>{`likes: ${numberOfLikes}`}</p><p>{`dislikes: ${numberOfDislikes}`}</p></div>
             <p>{watch.channel.title}</p>
             <p>{numberOfSubs} subs</p>
             {subscribing ? <button onClick={subscribe}>Unsubscribe</button> : <button onClick={subscribe}>Subscribe</button>}
